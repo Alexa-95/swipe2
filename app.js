0f86f0222df61f5dbb293b0f997c3dee0be9c88c
Sobańska Aleksandra <aleksandra.sobanska@grupawp.pl>
2019-12-19 15:25:48 +0100

    3nd c

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 var swiperH = new Swiper('.swiper-container-h', {
     spaceBetween: 50,
-    loop: true,
+    // loop: true,
     pagination: {
         el: '.swiper-pagination-h',
         clickable: true,
-    }
-    ,
+    },
     history: {
         key: '',
-      }
-});
-var swiperV = new Swiper('.swiper-container-v', {
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
+  var swiperV = new Swiper('.swiper-container-v', {
     direction: 'vertical',
     spaceBetween: 50,
     mousewheel: true,
+    mousewheelControl: true,
     pagination: {
         el: '.swiper-pagination-v',
         clickable: true,
     },
-});
+    history: {
+      key:  '',
+    }
+  });
+  
